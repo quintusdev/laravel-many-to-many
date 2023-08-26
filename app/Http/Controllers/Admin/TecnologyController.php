ba3edd441b9db104d06dfc49d200fd6a576dc489
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-08-26 12:40:40 +0200

    [FEATURES] Implementata index tecnology e use nel controller

--- app/Http/Controllers/Admin/TecnologyController.php
+++ app/Http/Controllers/Admin/TecnologyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Tecnology;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTecnologyRequest;
 use App\Http\Requests\UpdateTecnologyRequest;
 
